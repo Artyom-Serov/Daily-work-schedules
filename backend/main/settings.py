66e24d771161cc0447e1bf2b376815f6d797f918
Artyom-Serov <artyomserov@yandex.ru>
2024-06-06 06:30:17 +0500

    Setting up redirects to the main page

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -89,3 +89,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR.parent / 'frontend/static']
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
