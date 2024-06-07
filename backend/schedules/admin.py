33f371e4148775ef7963df71f627af11861de8d9
Artyom Serov(ws) <artyomserov@yandex.ru>
2024-06-07 13:48:09 +0500

    Setting up models for the admin panel

--- backend/schedules/admin.py
+++ backend/schedules/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Schedule, Work, Resource
 
-# Register your models here.
+
+@admin.register(Schedule)
+class ScheduleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'last_modified', 'description')
+    search_fields = ('title', 'author__username')
+    list_filter = ('last_modified', 'author')
+
+
+@admin.register(Work)
+class WorkAdmin(admin.ModelAdmin):
+    list_display = ('name', 'start_date', 'end_date', 'schedule')
+    search_fields = ('name', 'schedule__title')
+    list_filter = ('start_date', 'end_date', 'schedule')
+
+
+@admin.register(Resource)
+class ResourceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'unit')
+    search_fields = ('name',)
+    list_filter = ('unit', 'name')
