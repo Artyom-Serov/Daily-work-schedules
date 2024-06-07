3b23bcb6ea65c17df3e0899654f2de2ac67a3031
Artyom Serov(ws) <artyomserov@yandex.ru>
2024-06-07 14:00:42 +0500

    Setting up the main page

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -2,8 +2,9 @@
 Main URL configuration for "Daily work schedules" project.
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include('schedules.urls')),
 ]
--- backend/schedules/views.py
+++ backend/schedules/views.py
@@ -1,2 +1,11 @@
 from django.shortcuts import render
+from .models import Schedule
 
+
+def schedule_list(request):
+    schedules = Schedule.objects.all()
+    return render(
+        request,
+        'schedules/schedule_list.html',
+        {'schedule': schedules}
+    )
