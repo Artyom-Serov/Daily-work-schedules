bdab7dfe24cc05c061e7f8e55949f83e8a703a51
Artyom Serov(ws) <artyomserov@yandex.ru>
2024-06-08 15:36:21 +0500

    Templates for authorization and registration are configured

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('profile/', views.profile, name='profile'),
+    path('signup/', views.signup, name='signup'),
+    path('edit_profile/', views.edit_profile, name='edit_profile'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
 ]
