c71f423732cbef079925c29e78e6e1aa3cb6f8fa
Artyom Serov(ws) <artyomserov@yandex.ru>
2024-06-13 20:20:19 +0500

    Configuring the admin panel for schedules

--- backend/schedules/admin.py
+++ backend/schedules/admin.py
@@ -1,19 +1,35 @@
 from django.contrib import admin
-from .models import Schedule, Work
+from .models import Schedule, Work, Resource
+
+
+class ResourceInline(admin.TabularInline):
+    model = Resource
+    extra = 1
+
+
+class WorkInline(admin.TabularInline):
+    model = Work
+    extra = 1
 
 
 @admin.register(Schedule)
 class ScheduleAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'last_modified', 'description')
-    search_fields = ('title', 'author__username')
-    list_filter = ('last_modified', 'author')
+    list_display = ('title', 'author', 'last_modified')
+    search_fields = ('title', 'author__username', 'description')
+    list_filter = ('author', 'last_modified')
+    inlines = [WorkInline]
 
 
 @admin.register(Work)
 class WorkAdmin(admin.ModelAdmin):
-    list_display = (
-        'name', 'start_date', 'end_date', 'schedule',
-        'resource_name', 'resource_unit', 'resource_quantity'
-    )
-    search_fields = ('name', 'schedule__title', 'resource_name')
-    list_filter = ('start_date', 'end_date', 'schedule')
+    list_display = ('name', 'schedule', 'start_date', 'end_date')
+    search_fields = ('name', 'schedule__title')
+    list_filter = ('start_date', 'end_date')
+    inlines = [ResourceInline]
+
+
+@admin.register(Resource)
+class ResourceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'quantity', 'unit', 'work')
+    search_fields = ('name', 'work__name')
+    list_filter = ('unit',)
