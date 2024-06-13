1527056115cdeff09ea00b3464dfd5ef4fb32c83
Artyom Serov(ws) <artyomserov@yandex.ru>
2024-06-13 20:19:21 +0500

    Setting up URLs for schedules

--- backend/schedules/urls.py
+++ backend/schedules/urls.py
@@ -5,9 +5,14 @@ app_name = 'schedules'
 
 urlpatterns = [
     path('', views.schedule_list, name='schedule_list'),
+    path('create/', views.schedule_create, name='schedule_create'),
     path('<int:pk>/', views.schedule_detail, name='schedule_detail'),
-    path('new/', views.schedule_create, name='schedule_create'),
-    path('<int:pk>/edit/', views.schedule_edit, name='schedule_edit'),
+    path('<int:pk>/update/', views.schedule_update, name='schedule_update'),
     path('<int:pk>/delete/', views.schedule_delete, name='schedule_delete'),
-    path('work/<int:pk>/delete/', views.work_delete, name='work_delete'),
+    path('work/create/<int:schedule_pk>/', views.work_create, name='work_create'),
+    path('work/update/<int:pk>/', views.work_update, name='work_update'),
+    path('work/delete/<int:pk>/', views.work_delete, name='work_delete'),
+    path('resource/create/<int:work_pk>/', views.resource_create, name='resource_create'),
+    path('resource/update/<int:pk>/', views.resource_update, name='resource_update'),
+    path('resource/delete/<int:pk>/', views.resource_delete, name='resource_delete'),
 ]
